40cf84533670bb3c8a66be40f9b24acb3c0359c9
Jithukuttan MM <jithukuttanmm@Jithukuttans-MacBook-Air.local>
2021-05-09 19:37:51 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("./db/mongoose"); //to ensure it runs paralell
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/tasks");
 const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
+app.options("*task-manager*", cors());
 
 app.use(express.json()); //get req params as json
 app.use(userRouter);
